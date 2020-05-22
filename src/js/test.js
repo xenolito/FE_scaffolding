19a452afa0f529b31b6e2b35d2a4fa5c12b116a4
xenolito <oscar.rey.tajes@gmail.com>
2020-05-22 21:36:00 +0200

    security updates for packages

--- src/js/test.js
+++ src/js/test.js
@@ -1,22 +1,29 @@
-'use strict';
+"use strict";
 
 //block scoped "let" declaration
 const sentences = [
-	{ subject: 'Javascript', verb: 'is', adj: 'great' },
-	{ subject: 'Dinosaurs', verb: 'are', adj: 'Gooone' }
+  { subject: "Javascript", verb: "is", adj: "great" },
+  { subject: "Dinosaurs", verb: "are", adj: "Gooone" }
 ];
 
 //object destructuring
 function say({ subject, verb, adj }) {
-	console.log(`${subject} ${verb} ${adj}`);
+  return `${subject} ${verb} ${adj}`;
 }
 
 for (let s of sentences) {
-	say(s);
+  say(s);
 }
 
 (function() {
-	$(window).on('load', function() {
-		console.log(`hola que ase jquery: ${$('.testing').attr('class')}`);
-	});
+  $(window).on("load", function() {
+    const pp = (str, sufix) => `Hola que ASE= ${str} con sufijo= ${sufix}`;
+
+    console.log(
+      "%c" + `\ud83e\uddf2 hola que ase jquery: ${$(".testing").attr("class")}`,
+      "color:#343434;font-size:16px;font-family:Helvetica,Arial;"
+    );
+    let r = say(sentences[0]);
+    $("#result").html(pp("NAMCO", "SL"));
+  });
 })();
